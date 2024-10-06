27b935dd3c491353c8d1918f178978372bf49284
Valres <104681026+ValresMC@users.noreply.github.com>
2024-10-06 03:53:09 +0200

    Update AtomSanctions.php

--- src/Valres/AtomSanctions/AtomSanctions.php
+++ src/Valres/AtomSanctions/AtomSanctions.php
@@ -5,10 +5,17 @@ declare(strict_types = 1);
 namespace Valres\AtomSanctions;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
 
 class AtomSanctions extends PluginBase
 {
+    use SingletonTrait;
+
     public function onEnable(): void {
         $this->saveDefaultConfig();
+    }
+
+    protected function onLoad(): void {
+        self::setInstance($this);
     }
 }
